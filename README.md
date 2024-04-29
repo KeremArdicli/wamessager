4e0ebdf2dfad464213918809a792aa5a167cc8b6
KEREM ARDICLI <keremardicli@gmail.com>
2024-04-29 14:00:31 +0300

    added English explanation messages

--- README.md
+++ README.md
@@ -9,3 +9,12 @@
     - webdriver-manager
     - pyperclip
 
+### Usage
+- Webdriver will open a chrome window with web.whatsapp.com
+- Once page is open you need to scan QR code to link your device.
+- After you link your device and the WhatsApp page is ready to use, click ENTER. Script will wait for your prompt as this process duration can vary significantly from user to user. this will be done once for each use of the script.
+- Once ENTER is pressed, script will iterate through the CSV lines.
+- For the first message, script will ask you to press ENTER again. This is to make sure conversation screenşs loaded. For the next messages, script will wait for 12 sec and send automatically.
+- You can change 12 sec in the codes.
+- Every successful message will be printed on the console.
+- Once all is done, driver will quit itself.
--- final.py
+++ final.py
@@ -8,47 +8,54 @@ import pyperclip
 def copy2clip(txt):
     pyperclip.copy(txt)
 
-# CSV dosyasının adı ve yolu
+# CSV file directory / name
 csv_file = "mesajlar.csv"
 
-# Chrome WebDriver'ı başlat
+# Start Chrome WebDriver
 option = webdriver.ChromeOptions()
 option.add_argument("start-maximized")
 driver = webdriver.Chrome(options=option)
 
-# WhatsApp Web sitesini aç
+# Open WhatsApp Web
 driver.get("https://web.whatsapp.com/")
-input("WhatsApp QR kodunu tarattıktan sonra Enter tuşuna basın...")
+input("Press enter after you link your device and WhatsAppis raedy...")
 
-# CSV dosyasını oku ve mesajları gönder
+# Read CSV file for numbers and massages
 with open(csv_file, newline='\n', encoding='utf-8') as csvfile:
-    reader = csv.reader(csvfile) # Varsayılan olarak virgülle ayrıldığını belirtiyoruz
+    reader = csv.reader(csvfile) 
     #reader = csv.reader(csvfile)
     for i, row in enumerate(reader):
-        phone_number = row[0]  # İlk sütun telefon numarasını içerir
-        message = row[1].replace("\\n", "\n")  # İkinci sütun mesajı içerir
+        phone_number = row[0]  # first column mobile phone number
+        message = row[1].replace("\\n", "\n")  # second column message
 
-        # WhatsApp mesaj gönderme URL'sini oluştur
+        # We start chat with user's phone number. 
+        # we dont add '&text={message}' at the end of url
+        # bcs we need to send message with formatting
         url = f"https://web.whatsapp.com/send?phone={phone_number}"
 
-        # URL'yi aç
         driver.get(url)
         
+        # Wait for the message box to load. 
+        # This will be manuel for the first time as it takes longer. 
+        # for the next iterations, script will wait for 12 secs.
+        # if it is not enough for your case, you can increase or decrease the time
         if i == 0:
-            input("Mesaj göndermek için ENTER'a basın...")
+            input("Press ENTER after meeage screen is loaded...")
         else:
-            sleep(12)  # Yüklenmesini beklemek için biraz zaman ver
-        # Gönder butonunu bul ve tıkla
+            sleep(12)  # Wait for some time to load conversation screen
+        # Copy message to clipboard for formatting purposes and then paste it to message box
         copy2clip(message)
         element = driver.switch_to.active_element
     
-    # Simulate keyboard shortcut to paste (Ctrl + V)
+        # Simulate keyboard shortcut to paste (Ctrl + V)
         element.send_keys(Keys.CONTROL, 'v')
+        
+        # Click button to send message
         click_btn = driver.find_element(By.CLASS_NAME, 'x1lfpgzf')
         click_btn.click()
-        # Bir sonraki mesaj için biraz bekleyelim
+        # Wait a short period of time for the next message
         sleep(1)
-        print(f"{phone_number} numarasına mesaj gönderildi")
+        print(f"{phone_number}: message has been sent!")
 
-# İşlem tamamlandıktan sonra tarayıcıyı kapat
+# End driver after all is done
 driver.quit()
